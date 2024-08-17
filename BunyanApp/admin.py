40b5b6f589ba4e7db0c47367b155a40caf9d7296
Reem Joudeh <joudehreem@gmail.com>
2024-08-17 23:03:51 +0300

    updated admin

--- BunyanApp/admin.py
+++ BunyanApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Patient)
+admin.site.register(Therapist)
+admin.site.register(Choice)
+admin.site.register(Response)
+admin.site.register(Assessment)
+admin.site.register(Specialization)
+admin.site.register(Appointment)
--- BunyanApp/models.py
+++ BunyanApp/models.py
@@ -1,76 +1,84 @@
 from django.db import models
 
-# Create your models here.
-
-class Role(models.Model):
-    name = models.CharField(max_length=50)
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=225)
+    password = models.CharField(max_length=225)
+    dob = models.DateField()
+    mobile = models.CharField(max_length=255)
+    gender = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return self.name
+        return f'{self.first_name} {self.last_name}'
 
-class User(models.Model):
-  first_name =  models.CharField(max_length=255)
-  last_name = models.CharField(max_length=255)
-  username = models.CharField(max_length=255)
-  email = models.EmailField(max_length=225)
-  password = models.CharField(max_length=225)
-  confirm_password = models.CharField(max_length=225)  
-  dob = models.DateField()
-  role = models.ForeignKey(Role, on_delete=models.CASCADE)
-  mobile = models.CharField(max_length=255)
-  gender = models.CharField(max_length=20)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  def __str__(self):
-        return f'{self.first_name} {self.last_name} ({self.role.name})'
+class Patient(User):
+    medical_history = models.TextField()
+    
+    def __str__(self):
+        return f'Patient: {self.first_name} {self.last_name}'
 
-class Patient(models.Model):
-  ##
-  description = models.TextField
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  def __str__(self):
-    pass
-
-class Therapy(models.Model):
-  ##
-  available_time = models.DateTimeField()
-  experience = models.TextField()
-  location = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  def __str__(self):
-    pass
-
-class Assessment(models.Model):
-  patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-  mental_wellness = models.IntegerField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  def __str__(self):
-    pass
+class Therapist(User):
+    available_time = models.DateTimeField()
+    experience = models.TextField()
+    location = models.TextField()
+    
+    def __str__(self):
+        return f'Therapist: {self.first_name} {self.last_name}, Location: {self.location}'
 
 class Question(models.Model):
-  text = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  def __str__(self):
-    pass
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.text
 
-class Response(models.Model):## ManyToMany or forginkey
-  question = models.ForeignKey(Question, on_delete=models.CASCADE)
-  answer = models.TextField()
-  score = models.IntegerField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  def __str__(self):
-    pass
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    points = models.IntegerField()  # Points from 1 to 5
+    
+    def __str__(self):
+        return f'{self.text} ({self.points} points)'
 
-class Appointment(models.Mode):
-  patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-  therapy = models.ForeignKey(Therapy, on_delete=models.CASCADE)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  def __str__(self):
-    pass
+class Response(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'Response to {self.question}: {self.selected_choice.text}'
+
+class Assessment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    responses = models.ManyToManyField(Response)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    mental_wellness = models.IntegerField() 
+    def __str__(self):
+        return f'Assessment for {self.patient}'
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    therapist = models.ForeignKey(Therapist, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'Appointment with {self.therapist} for {self.patient}'
+
+class Specialization(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    therapists = models.ManyToManyField(Therapist, related_name="specializations")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
