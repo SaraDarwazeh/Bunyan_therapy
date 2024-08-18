6e2ff4b299b1ae94600527deb30622ae86d5bd99
Reem Joudeh <joudehreem@gmail.com>
2024-08-18 19:40:14 +0300

    Add CRUD

--- BunyanApp/models.py
+++ BunyanApp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-import bycrypt
 import re
+import bcrypt
 from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
 
 # class Manger to register and login user
 class UserManger(models.Manager):
+    #validations for user
     def register(self,postData):
         errors ={}
         # validation First Name ## change how may chracter for all and msg
@@ -55,10 +56,9 @@ class UserManger(models.Manager):
         # \s?: Allows an optional space after the country code.
         # \d{7,15}$: Matches 7 to 15 digits for the rest of the phone number.
         return errors
-
+    # Authorize email and password
     def login(self,postData):
         errors = {}
-        # Authorize email and password
         try:  
             user = User.objects.get(email=postData['email'])
         except ObjectDoesNotExist:
@@ -102,7 +102,7 @@ class Therapist(User):
     def __str__(self):
         return f'Therapist: {self.first_name} {self.last_name}, Location: {self.location}'
 
-class Question(models.Model):
+class Question(models.Model):#Add from admin panel
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -110,7 +110,7 @@ class Question(models.Model):
     def __str__(self):
         return self.text
 
-class Choice(models.Model):
+class Choice(models.Model):#Add from admin panel
     question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     points = models.IntegerField()  # Points from 1 to 5
@@ -132,9 +132,9 @@ class Response(models.Model):
 
 class Assessment(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    mental_wellness = models.IntegerField() 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    mental_wellness = models.IntegerField() 
     
     def __str__(self):
         return f'Assessment for {self.patient}'
@@ -149,7 +149,7 @@ class Appointment(models.Model):
     def __str__(self):
         return f'Appointment with {self.therapist} for {self.patient}'
 
-class Specialization(models.Model):
+class Specialization(models.Model): #Add from admin panel
     title = models.CharField(max_length=45)
     description = models.TextField()
     therapists = models.ManyToManyField(Therapist, related_name='specializations')
@@ -158,3 +158,69 @@ class Specialization(models.Model):
     
     def __str__(self):
         return self.title
+
+#All Patients
+def all_patients():
+    return Patient.objects.all()
+#All therapists
+def all_therapist():
+    return Therapist.objects.all()
+# patient ID
+def patient(patient_id):
+    return Patient.objects.get(id=patient_id)
+#therapist ID
+def therapist(therapist_id):
+    return Therapist.objects.get(id=therapist_id)
+#Create User 
+def create_patient(POST):
+    password = POST['password']
+    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    return Patient.objects.create(
+        first_name = POST['first_name'],
+        last_name = POST['last_name'],
+        username = POST['username'],
+        email = POST['email'],
+        password = hashed_password,
+        dob = POST['dob'],
+        mobile = POST['mobile'],
+        gender = POST['gender'],
+        medical_history = POST['medical_history'],
+    )
+#update information of patient,Can we make change pass and use to forget password
+def update_patient(POST,patient_id):
+    patient=patient(patient_id)
+    patient.first_name = POST['first_name']
+    patient.last_name = POST['last_name']
+    patient.username = POST['username']
+    patient.email = POST['email']
+    patient.dob = POST['dob']
+    patient.mobile = POST['mobile']
+    patient.save()
+# deactivate of patient account
+def deactivate_user(POST):
+    patient = patient(POST['patient_id'])
+    patient.delete()
+# response patient
+def response_patient():
+    pass
+# add appointment and updated the appointment and deleted
+def appointment(appointment_id):
+    return Appointment.objects.get(id=appointment_id)
+def create_appointment():
+    pass
+def update_appointment():
+    pass
+def remove_appointment(POST):
+    appointment=appointment(id=POST['appointment_id'])
+    appointment.delete()
+    
+#QUERY NEED
+
+# show all appointments 
+# for patients side all appointments
+# and the doctors side all appointments
+
+# all data for Response and assessments
+# add function for specializations appear with therapist
+# search how to calculate the score of mental wellness
+#  validation for appointment and response
