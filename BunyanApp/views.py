94bd67e61d0e9d73c4f00e04f9903d36e3a6c70a
Majd Abu Sada <abusadamajd@gmail.com>
2024-08-20 09:30:49 +0300

    updated_forms

--- BunyanApp/views.py
+++ BunyanApp/views.py
@@ -9,7 +9,8 @@ def index(request):
   }
   return render(request,'index.html',context)
 
-
+def test(request):
+  return render(request,'test.html')
 # def index(request):
 #     if request.method == "POST":
 #         errors = User.objects.basic_validator(request.POST)
